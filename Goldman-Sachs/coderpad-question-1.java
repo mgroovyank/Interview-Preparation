8f4e632859101eecddf00ce07cea35b7d63dda64
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-01 15:45:13 +0530

    Create coderpad-question-1.java

---
+++ Goldman-Sachs/coderpad-question-1.java
@@ -0,0 +1,9 @@
+// Truck - 4tyres
+// Each tyre has 10 spikes
+// start position of tyre - [0 , 1, 2, 2] - present at spike
+// end positon of tyre - [4, 5, 6, 7] - reach this spike
+// each tyre can either move forward/backward
+// one second you can move only one tyre
+// Blocked states - [[1,2,3,4], [5,6,7,8], [2,4,5,6]]
+// Min time for tyres to reach end position??
+
