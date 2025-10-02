f6789d4bc8d5b71c6172fa9198af279044e63209
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-02 18:55:02 +0530

    Update sqrtx.java

--- Goldman-Sachs/Coderpad/sqrtx.java
+++ Goldman-Sachs/Coderpad/sqrtx.java
@@ -19,3 +19,30 @@ class Solution {
         return x==1 ? 1 : low; 
     }
 }
+
+class Solution {
+    public static double sqrtWithPrecision(double n) {
+        double low = 0;
+        double high = n;
+        double mid;
+
+        while (high - low > 0.01) {
+            mid = low + (high - low) / 2;
+            double square = mid * mid;
+
+            if (square < n) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+
+        // Use high and floor to ensure rounding down to 2 decimal places
+        return Math.floor(high * 100.0) / 100.0;
+    }
+
+    public static void main(String[] args) {
+        double n = 10;
+        System.out.println("Square root of " + n + " is: " + sqrtWithPrecision(n));
+    }
+}
