bd2e23e6f8432d49187c3a8209fd8f069f53ead2
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-15 19:30:59 +0530

    Update apply-operations-to-maximize-frequency-score.java

--- Goldman-Sachs/Coderpad/apply-operations-to-maximize-frequency-score.java
+++ Goldman-Sachs/Coderpad/apply-operations-to-maximize-frequency-score.java
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/apply-operations-to-maximize-frequency-score/
 /* 
 1. if I choose a number which is already present in nums for the becoming the most frequent, I save on k. I can use those  savings to make other numbers equal to k.
 2. Let' say I choose a number greater than maxValue as K, i.e. maxValue+1, then I have to make even the max value equal to k. And not only that, I would need k to make all elements which could become equal to maxValue, also to add 1 to make them as maxValue+1.
