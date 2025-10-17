4b57e7835e444cbe2dd77f72e13403afba124b0e
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-17 22:41:20 +0530

    Update geeks-training.java

--- leet-code/dynamic-programming/geeks-training.java
+++ leet-code/dynamic-programming/geeks-training.java
@@ -80,3 +80,64 @@ class Solution {
         return Math.max(dp[numDays-1][0], Math.max(dp[numDays-1][1], dp[numDays-1][2]));
     }
 }
+
+// Converting from recursion to tabulation - bottom up approach
+class Solution {
+    public int maximumPoints(int arr[][]) {
+        // recursion - bottom up approach
+        // bottom up approach is iterative in nature
+        // 1. declare similar size dp array
+        // 2. For base case, will be executed for 4 different 'last"
+        int numDays = arr.length;
+        int[][] dp = new int[numDays][4];
+        // using base case
+        dp[0][0] = Math.max(arr[0][1], arr[0][2]);
+        dp[0][1] = Math.max(arr[0][0], arr[0][2]);
+        dp[0][2] = Math.max(arr[0][0], arr[0][1]);
+        dp[0][3] = Math.max(arr[0][0], Math.max(arr[0][1], arr[0][2]));
+        
+        // using normal flow logic
+        for(int day=1;day<numDays;day++){
+            for(int last=0;last<4;last++){
+                for(int task=0;task<3;task++){
+                    if(task != last){
+                        dp[day][last] = Math.max(dp[day][last], arr[day][task] + dp[day-1][task]);
+                    }
+                }
+            }
+        }
+        return dp[numDays-1][3];
+    }
+}
+
+// Space Optimization
+// Only Today(i) and Previous Day(i-1) info is required, not of all days before ith day
+// User function Template for Java
+
+class Solution {
+    public int maximumPoints(int arr[][]) {
+        // Space Optimization
+        int numDays = arr.length;
+        int[] dp = new int[4];
+        // using base case
+        dp[0] = Math.max(arr[0][1], arr[0][2]);
+        dp[1] = Math.max(arr[0][0], arr[0][2]);
+        dp[2] = Math.max(arr[0][0], arr[0][1]);
+        dp[3] = Math.max(arr[0][0], Math.max(arr[0][1], arr[0][2]));
+        
+        // using normal flow logic
+        for(int day=1;day<numDays;day++){
+            int[] temp = new int[4];
+            Arrays.fill(temp, 0);
+            for(int last=0;last<4;last++){
+                for(int task=0;task<3;task++){
+                    if(task != last){
+                        temp[last] = Math.max(temp[last],  arr[day][task] + dp[task]);
+                    }
+                }
+            }
+            dp = temp;
+        }
+        return dp[3];
+    }
+}
