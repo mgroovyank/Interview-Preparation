1855832780ddc638aeb31a272eaa4b271de6bdce
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-19 15:50:51 +0530

    Create unique-paths-ii.java

---
+++ leet-code/dynamic-programming/unique-paths-ii.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/unique-paths-ii/
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        // Recursion - Top Down Approach
+        // f(i,j) = f(i-1, j) * 1 + f(i, j-1) * 1
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        return uniquePathsTo(obstacleGrid, m-1, n-1, dp);
+    }
+
+    private int uniquePathsTo(int[][] obstacleGrid, int i, int j, int[][] dp){
+        if(i==0 && j==0){
+            return 1;
+        }
+        if(i<0 || j<0 || (obstacleGrid[i][j]==1)){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        return dp[i][j] = uniquePathsTo(obstacleGrid, i-1, j, dp) + uniquePathsTo(obstacleGrid, i, j-1, dp);
+    }
+}
