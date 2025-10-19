2e42ce5843a97e794004b2e26f2213a9cc9808cd
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-19 14:02:22 +0530

    Update unique-paths.java

--- leet-code/dynamic-programming/unique-paths.java
+++ leet-code/dynamic-programming/unique-paths.java
@@ -52,3 +52,33 @@ class Solution {
         return dp[i][j];
     }
 }
+
+// Time Complexity: O(M*N*1)
+// Space Complexity: O(M*N)
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // Tabulation, Bottom Up Approach
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        // iteration on all states reached via recursion
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                // "do stuff" on indexes code
+                if(i==0 && j==0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if(i-1 >= 0){
+                    up = dp[i-1][j];
+                }
+                if(j-1>=0){
+                    left = dp[i][j-1];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
