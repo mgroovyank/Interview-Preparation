87d0c07e5db752bf7e61b80634e4d7a2707238be
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-21 14:07:08 +0530

    Create cherry-pickup-ii.java

---
+++ leet-code/dynamic-programming/cherry-pickup-ii.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/cherry-pickup-ii/
+
+// Time Complexity: 9^(n*m*m)
+// number of states = n*m*m => for each state you call recursion for 9 deltas
+// Space Complexity: O(n) - recursion stack
+
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        // Top Down Approach - Fixed Starting Point, Variable Ending Point
+        // Way 1 - Move R1 first across grid -> get max Path -> mark zero on that path -> move R2 similarly
+        // Way 1 - little more complex due to tracking of path
+        // Way 2 - move both together
+        int n = grid.length;
+        int m = grid[0].length;
+        return maxCherryPickupFrom(grid, 0, 0, m-1);
+    }
+
+    private int maxCherryPickupFrom(int[][] grid, int i12, int j1, int j2){
+        // base case
+        if(j1<0 || j2<0 || j1>=grid[0].length || j2>=grid[0].length){
+            return Integer.MIN_VALUE;
+        }
+        if(i12 == grid.length-1){
+            if(j1 == j2){
+                return grid[i12][j1];
+            }
+            return grid[i12][j1] + grid[i12][j2];
+        }
+        int[] delta = new int[]{-1, 0, 1};
+        int maxi = Integer.MIN_VALUE;
+        for(int d1:delta){
+            for(int d2:delta){
+                int temp = grid[i12][j1] + grid[i12][j2];
+                if(j1 == j2){
+                    temp = grid[i12][j1];
+                }
+                maxi = Math.max(maxi, temp + maxCherryPickupFrom(grid, i12+1, j1+d1, j2+d2));
+            }
+        }
+        return maxi;
+    }
+}
