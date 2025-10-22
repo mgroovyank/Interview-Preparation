043cc0e1c0d40212fe8448e72ddde67df4d927d4
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-22 17:14:16 +0530

    Update perfect-sum-problem.java

--- leet-code/dynamic-programming/perfect-sum-problem.java
+++ leet-code/dynamic-programming/perfect-sum-problem.java
@@ -30,3 +30,51 @@ class Solution {
         return  dp[i][target] = take + notTake;
     }
 }
+
+// Tabulation
+class Solution {
+    // Function to calculate the number of subsets with a given sum
+    public int perfectSum(int[] nums, int target) {
+        // Tabulation - Bottom Up Approach
+        int n = nums.length;
+        int[][] dp = new int[n][target+1];
+        for(int sum=0;sum<=target;sum++){
+            dp[0][sum] = 0;
+            dp[0][sum] = sum == nums[0] ? dp[0][sum] + 1 : dp[0][sum];
+        }
+        dp[0][0] += 1; // not taking
+        for(int i=1;i<n;i++){
+            for(int sum=0;sum<=target;sum++){
+                int take = sum >= nums[i] ? dp[i-1][sum-nums[i]] : 0;
+                int notTake = dp[i-1][sum];
+                dp[i][sum] = take + notTake;
+            }
+        }
+        return dp[n-1][target];
+    }
+}
+
+// Space Optimized
+class Solution {
+    // Function to calculate the number of subsets with a given sum
+    public int perfectSum(int[] nums, int target) {
+        // Tabulation - Bottom Up Approach
+        int n = nums.length;
+        int[] prev = new int[target+1];
+        for(int sum=0;sum<=target;sum++){
+            prev[sum] = 0;
+            prev[sum] = sum == nums[0] ? prev[sum] + 1 : prev[sum];
+        }
+        prev[0] += 1;
+        for(int i=1;i<n;i++){
+            int[] curr = new int[target+1];
+            for(int sum=0;sum<=target;sum++){
+                int take = sum >= nums[i] ? prev[sum-nums[i]] : 0;
+                int notTake = prev[sum];
+                curr[sum] = take + notTake;
+            }
+            prev = curr;
+        }
+        return prev[target];
+    }
+}
