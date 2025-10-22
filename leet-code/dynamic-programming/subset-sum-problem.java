d90527929450b76d36b3b4fada7c1ee7c7adbdf4
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-22 12:08:11 +0530

    Update subset-sum-problem.java

--- leet-code/dynamic-programming/subset-sum-problem.java
+++ leet-code/dynamic-programming/subset-sum-problem.java
@@ -106,3 +106,41 @@ class Solution {
         return dp[n-1][sum] == 1;
     }
 }
+
+// Time Complexity: O(n*sum)
+// Space Complexity: O(sum)
+class Solution {
+
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // Reduce recursion stack space - Bottom Up Approach
+        // Tabulation
+        int n = arr.length;
+        int[] prev = new int[sum+1];
+        // sum is zero, always true
+        for(int i=0;i<n;i++){
+            prev[0] = 1;
+        }
+        // i=0, then 1 if arr[0]==sum
+        for(int s=1;s<=sum;s++){
+            prev[s] = (s == arr[0]) ? 1 : 0;
+        }
+        for(int i=1;i<n;i++) {
+            int[] curr = new int[sum+1];
+            curr[0] = 1;
+            for(int s=1;s<=sum;s++) {
+                int noTake = prev[s];
+                int take = 0;
+                if(s >= arr[i]){
+                    take = prev[s-arr[i]];
+                }
+                if(noTake==1 || take==1){
+                    curr[s] = 1;
+                }else{
+                    curr[s] = 0;
+                }   
+            }
+            prev = curr;
+        }
+        return prev[sum] == 1;
+    }
+}
