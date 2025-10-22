f987a3b82aa528eca99ba752432a0dec382355f5
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-22 11:29:59 +0530

    Update subset-sum-problem.java

--- leet-code/dynamic-programming/subset-sum-problem.java
+++ leet-code/dynamic-programming/subset-sum-problem.java
@@ -24,3 +24,49 @@ class Solution {
                || isSubsetSumTillIthIdx(arr, i-1, sum-arr[i]);
     }
 }
+
+// DP
+// Time Complexity: O(n*sum)
+// Space Complexity: O(n*sum) + O(n)
+class Solution {
+
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // Recursion - Top Down Approach
+        // f(i, target) = subsequence till ith index with sum as target
+        // every leaf node will be f(n-1, target)
+        // overlapping subproblems
+        int n = arr.length;
+        int[][] dp = new int[n][sum+1];
+        for(int[] d: dp){
+            Arrays.fill(d, -1);
+        }
+        return isSubsetSumTillIthIdx(arr, n-1, sum, dp) == 1;
+    }
+    
+    static int isSubsetSumTillIthIdx(int[] arr, int i, int sum, int[][] dp){
+        // base case
+        if(i<0){
+            return 0;
+        }
+        if(sum == 0){
+            return 1;
+        }
+        if(i == 0){
+            return arr[0] == sum ? 1 : 0;
+        }
+        if(dp[i][sum] != -1){
+            return dp[i][sum];
+        }
+        int noTake = isSubsetSumTillIthIdx(arr, i-1, sum, dp);
+        // if you take without this condition, then sum will become negative index
+        // for dp
+        int take = 0;
+        if(sum >= arr[i]){
+            take = isSubsetSumTillIthIdx(arr, i-1, sum-arr[i], dp);
+        }
+        if(noTake==1 || take==1){
+            return dp[i][sum] = 1;
+        }
+        return dp[i][sum] = 0;
+    }
+}
