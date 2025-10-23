d49a6e8199fcfda3539a55366904ccb09be55957
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-23 11:13:29 +0530

    Create coin-change.java

---
+++ leet-code/dynamic-programming/coin-change.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/coin-change/
+
+// Recursion time complexity: O(2^n) - even more than that
+
+// DP
+// Time Complexity: O(n*amount)
+// Space Complexity: O(n*amount) + O(amount)
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        // Infinite supplies
+        // Recursion - Top Down Approach
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int[] d: dp){
+            Arrays.fill(d, -1);
+        }
+        int ans = fewestCoinsTillIthIndex(coins, n-1, amount, dp);
+        if(ans == Integer.MAX_VALUE){
+            return -1;
+        }
+        return ans;
+    }
+    private int fewestCoinsTillIthIndex(int[] coins, int i, int amount, int[][] dp){
+        // base case
+        if(i == 0){
+            if(amount == 0){
+                return 0;
+            }
+            if(amount % coins[0] == 0){
+                return amount / coins[0];
+            }
+            return Integer.MAX_VALUE; // Impossible to achieve amount, don't return -1/0
+        }
+        if(dp[i][amount] != -1){
+            return dp[i][amount];
+        }
+        int notTake = fewestCoinsTillIthIndex(coins, i-1, amount, dp);
+        int take = Integer.MAX_VALUE;
+        if(amount >= coins[i]){
+            int temp = fewestCoinsTillIthIndex(coins, i, amount - coins[i], dp);
+            if(temp != Integer.MAX_VALUE){
+                take = 1 + temp;
+            }
+        }
+        return dp[i][amount] = Math.min(take, notTake);
+    }
+}
