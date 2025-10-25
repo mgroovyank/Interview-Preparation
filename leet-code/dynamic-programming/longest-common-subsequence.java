9a768633a708de4bb4f0323b25bc3b0e16a32c64
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-25 13:45:05 +0530

    Update longest-common-subsequence.java

--- leet-code/dynamic-programming/longest-common-subsequence.java
+++ leet-code/dynamic-programming/longest-common-subsequence.java
@@ -50,3 +50,33 @@ class Solution {
             longestCommonSubsequenceLenTillIthIdx(text1, text2, i1, i2-1, dp));
     }
 }
+
+// Tabulation
+// Time Complexity: O(M*N)
+// Space Complexity: O(M*N)
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        // compare subsequences on the go
+        // Bottom Up Approach - Tabulation
+        int l1 = text1.length();
+        int l2 = text2.length();
+        int[][] dp = new int[l1+1][l2+1];
+        // base case
+        for(int i1=0;i1<l1+1;i1++){
+            dp[i1][0] = 0;
+        }
+        for(int i2=0;i2<l2+1;i2++){
+            dp[0][i2] = 0;
+        }
+        for(int i1=1;i1<l1+1;i1++){
+            for(int i2=1;i2<l2+1;i2++){
+                if(text1.charAt(i1-1) == text2.charAt(i2-1)){
+                    dp[i1][i2] = 1 + dp[i1-1][i2-1];
+                }else {
+                    dp[i1][i2] = Math.max(dp[i1-1][i2], dp[i1][i2-1]); 
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+}
