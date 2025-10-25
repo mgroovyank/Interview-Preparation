67a6a653f36dd8a57709fa8d5b95a61e86e83b9f
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-25 11:47:14 +0530

    Update knapsack-with-duplicate-items.java

--- leet-code/dynamic-programming/knapsack-with-duplicate-items.java
+++ leet-code/dynamic-programming/knapsack-with-duplicate-items.java
@@ -23,8 +23,6 @@ class Solution {
     }
 }
 
-// User function Template for Java
-
 // Time Complexity: O(C*N)
 // Space Complexity: O(C*N) + O(N)
 class Solution {
@@ -52,5 +50,82 @@ class Solution {
             take = val[i] + maxProfitTillIthIdx(i, capacity-wt[i], wt, val, dp);
         }
         return dp[i][capacity] = Math.max(notTake,  take);
+    }
+}
+
+// Time Complexity: O(C*N)
+// Space Complexity: O(C*N)
+class Solution {
+    static int knapSack(int val[], int wt[], int capacity) {
+        // Bottom Up Approach - Tabulation
+        int n = val.length;
+        int[][] dp = new int[n][capacity+1];
+        for(int c=0;c<=capacity;c++){
+            dp[0][c] = (c / wt[0]) * val[0];
+        }
+        for(int i=1;i<n;i++){
+            for(int c=0;c<=capacity;c++){
+                int notTake = dp[i-1][c];
+                int take = 0;
+                if(c >= wt[i]){
+                    take = val[i] + dp[i][c-wt[i]];
+                }
+                dp[i][c] = Math.max(notTake,  take);
+            }
+        }
+        return dp[n-1][capacity];
+    }
+}
+
+// Time Complexity: O(C*N)
+// Space Complexity: O(C*2)
+class Solution {
+    static int knapSack(int val[], int wt[], int capacity) {
+        // Bottom Up Approach - Tabulation
+        // Sapace Optimization
+        int n = val.length;
+        int[] dp = new int[capacity+1];
+        for(int c=0;c<=capacity;c++){
+            dp[c] = (c / wt[0]) * val[0];
+        }
+        for(int i=1;i<n;i++){
+            int[] curr = new int[capacity+1];
+            for(int c=0;c<=capacity;c++){
+                int notTake = dp[c];
+                int take = 0;
+                if(c >= wt[i]){
+                    take = val[i] + curr[c-wt[i]];
+                }
+                curr[c] = Math.max(notTake,  take);
+            }
+            dp = curr;
+        }
+        return dp[capacity];
+    }
+}
+
+// Time Complexity: O(C*N)
+// Space Complexity: O(C)
+class Solution {
+    static int knapSack(int val[], int wt[], int capacity) {
+        // Bottom Up Approach - Tabulation
+        // Space Optimization
+        int n = val.length;
+        int[] curr = new int[capacity+1];
+        for(int c=0;c<=capacity;c++){
+            curr[c] = (c / wt[0]) * val[0];
+        }
+        for(int i=1;i<n;i++){
+            for(int c=0;c<=capacity;c++){
+                // from prev I just need value at c
+                int notTake = curr[c];
+                int take = 0;
+                if(c >= wt[i]){
+                    take = val[i] + curr[c-wt[i]];
+                }
+                curr[c] = Math.max(notTake,  take);
+            }
+        }
+        return curr[capacity];
     }
 }
