1b0a5500b187e9ca7eacb5cdd04ec8408e0419e2
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-26 12:23:57 +0530

    Update distinct-subsequences.java

--- leet-code/dynamic-programming/distinct-subsequences.java
+++ leet-code/dynamic-programming/distinct-subsequences.java
@@ -89,3 +89,93 @@ class Solution {
         return dp[i][j] = numDistinctTillIj(s, t, i-1, j, dp);
     }
 }
+
+// Tabulation
+class Solution {
+    public int numDistinct(String s, String t) {
+        // Tabulation - Bottom Up Approach
+        // f(i,j) = (f(i-1,j-1) + f(i-1, j)) or (f(i-1,j)) depending upon if s[i]==t[j] 
+        int sl = s.length();
+        int tl = t.length();
+        int[][] dp = new int[sl+1][tl+1];
+        // base case
+        for(int i=0;i<=sl;i++){
+            dp[i][0] = 1;
+        }
+        for(int j=1;j<=tl;j++){
+            dp[0][j] = 0;
+        }
+        // do stuff
+        for(int i=1;i<=sl;i++){
+            for(int j=1;j<=tl;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[sl][tl];
+    }
+}
+
+// Space Optimized
+class Solution {
+    public int numDistinct(String s, String t) {
+        // Tabulation - Bottom Up Approach
+        // f(i,j) = (f(i-1,j-1) + f(i-1, j)) or (f(i-1,j)) depending upon if s[i]==t[j] 
+        int sl = s.length();
+        int tl = t.length();
+        int[] prev = new int[tl+1];
+        // base case
+        for(int i=0;i<=sl;i++){
+            prev[0] = 1;
+        }
+        for(int j=1;j<=tl;j++){
+            prev[j] = 0;
+        }
+        // do stuff
+        for(int i=1;i<=sl;i++){
+            int[] curr = new int[tl+1];
+            curr[0] = 1;
+            for(int j=1;j<=tl;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    curr[j] = prev[j-1] +prev[j];
+                }else {
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+        return prev[tl];
+    }
+}
+
+// 1D Array Optimized
+class Solution {
+    public int numDistinct(String s, String t) {
+        // Tabulation - Bottom Up Approach
+        // f(i,j) = (f(i-1,j-1) + f(i-1, j)) or (f(i-1,j)) depending upon if s[i]==t[j] 
+        int sl = s.length();
+        int tl = t.length();
+        int[] prev = new int[tl+1];
+        // base case
+        for(int i=0;i<=sl;i++){
+            prev[0] = 1;
+        }
+        for(int j=1;j<=tl;j++){
+            prev[j] = 0;
+        }
+        // do stuff
+        for(int i=1;i<=sl;i++){
+            for(int j=tl;j>=1;j--){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    prev[j] = prev[j-1] +prev[j];
+                }else {
+                    prev[j] = prev[j];
+                }
+            }
+        }
+        return prev[tl];
+    }
+}
