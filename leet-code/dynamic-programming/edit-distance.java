6e6ff337b2957b6cb5ab457fa30437cf1cdf1e14
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-26 13:45:55 +0530

    Update edit-distance.java

--- leet-code/dynamic-programming/edit-distance.java
+++ leet-code/dynamic-programming/edit-distance.java
@@ -93,3 +93,34 @@ class Solution {
         minDistanceTillIj(word1, word2, i, j-1, dp))); // insert
     }
 }
+
+
+// With Tabulation, runtime can increase as now we populate all states, instead of just normal DP
+class Solution {
+    public int minDistance(String word1, String word2) {
+        // Tabulation
+        int w1 = word1.length();
+        int w2 = word2.length();
+        int[][] dp = new int[w1+1][w2+1];
+        // base case
+        for(int j=0;j<=w2;j++){
+            dp[0][j] = j;
+        }
+        for(int i=1;i<=w1;i++){
+            dp[i][0] = i;
+        }
+        for(int i=1;i<=w1;i++){
+            for(int j=1;j<=w2;j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], //delete
+                        Math.min(
+                            dp[i-1][j-1], // replace
+                            dp[i][j-1])); // insert
+                }
+            }
+        }
+        return dp[w1][w2];
+    }
+}
