391e2015664d1e76bfb409b3412439786fd87135
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-26 12:05:18 +0530

    Update distinct-subsequences.java

--- leet-code/dynamic-programming/distinct-subsequences.java
+++ leet-code/dynamic-programming/distinct-subsequences.java
@@ -57,3 +57,35 @@ class Solution {
         return dp[i][j] = numDistinctTillIj(s, t, i-1, j, dp);
     }
 }
+
+// Right Shift DP
+class Solution {
+    public int numDistinct(String s, String t) {
+        // Recursion Top Down Approach
+        // f(i,j) = (f(i-1,j-1) + f(i-1, j)) or (f(i-1,j)) depending upon if s[i]==t[j] 
+        int sl = s.length();
+        int tl = t.length();
+        int[][] dp = new int[sl+1][tl+1];
+        for(int[] d: dp){
+            Arrays.fill(d, -1);
+        }
+        return numDistinctTillIj(s, t, sl, tl, dp);
+    }
+
+    private int numDistinctTillIj(String s, String t, int i, int j, int[][] dp){
+        // base case
+        if(j == 0){
+            return 1;
+        }
+        if(i == 0){
+            return 0;
+        }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if(s.charAt(i-1) == t.charAt(j-1)){
+            return dp[i][j] = numDistinctTillIj(s, t, i-1, j-1, dp) + numDistinctTillIj(s, t, i-1, j, dp);
+        }
+        return dp[i][j] = numDistinctTillIj(s, t, i-1, j, dp);
+    }
+}
