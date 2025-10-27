2a7a40bb3a3107757709d5b465151ab1601e4b38
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-27 08:10:40 +0530

    Create wildcard-matching.java

---
+++ leet-code/dynamic-programming/wildcard-matching.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/wildcard-matching/
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        // Recursion - Top Down Approach
+        // f(i,j) = if pattern p till index j can fully represent string s till index i
+        // f(i,j) = f(i-1, j-1) | (f(i, j-1) || f(i-1, j))
+        int sl = s.length();
+        int pl = p.length();
+        return isMatchTillIndexIj(s, p, sl-1, pl-1)==1 ? true : false;
+    }
+
+    private int isMatchTillIndexIj(String s, String p, int i, int j){
+        // base case
+        if(i<0){
+            if(j<0){
+                return 1;
+            }
+            for(int k=0;k<=j;k++){
+                if(p.charAt(k) != '*'){ // even chars are a wildcard character
+                    return 0;
+                }
+            }
+            return 1;
+        }
+        if(j<0){
+            return 0;
+        }
+        char sc = s.charAt(i);
+        char pc = p.charAt(j);
+        if(pc == '?' ||  sc==pc){
+            return isMatchTillIndexIj(s, p, i-1, j-1);
+        }
+        if(pc == '*'){
+            return (isMatchTillIndexIj(s, p, i, j-1) + isMatchTillIndexIj(s, p, i-1, j)) > 0 ? 1 : 0;
+        }
+        return 0;
+    }
+}
