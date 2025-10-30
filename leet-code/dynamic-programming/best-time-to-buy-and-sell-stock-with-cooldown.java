48d2b6bfec3afd961192bc225ec20ade868c62aa
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-30 10:59:46 +0530

    Update best-time-to-buy-and-sell-stock-with-cooldown.java

--- leet-code/dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ leet-code/dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -31,3 +31,28 @@ class Solution {
         return dp[i][buyFlag] = Math.max(sell, notSell);
     }
 }
+
+// Tabulation - Bottom Up Approach
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        // after sell on ith day - i don't hold any stock on i+1th day, so I can't sell
+        // also i can't buy because of cooldown, so I just skip i+1th day after sell on ith day
+        // Tabulation
+        int n = prices.length;
+        int[][] dp = new int[n+2][2];
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        dp[n+1][0] = 0;
+        dp[n+1][1] = 0;
+        for(int i=n-1;i>=0;i--){
+            int buy = -prices[i] + dp[i+1][0];
+            int notBuy = dp[i+1][1];
+            dp[i][1] = Math.max(buy, notBuy);
+            int sell = prices[i] + dp[i+2][1];
+            int notSell = dp[i+1][0];
+            dp[i][0] = Math.max(sell, notSell);
+        }
+        return dp[0][1];
+    }
+}
