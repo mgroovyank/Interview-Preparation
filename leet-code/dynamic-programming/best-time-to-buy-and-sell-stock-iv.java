83adad2155e0c9aee1f4e25d94d49abe4d175ca1
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-30 10:37:23 +0530

    Update best-time-to-buy-and-sell-stock-iv.java

--- leet-code/dynamic-programming/best-time-to-buy-and-sell-stock-iv.java
+++ leet-code/dynamic-programming/best-time-to-buy-and-sell-stock-iv.java
@@ -22,3 +22,34 @@ class Solution {
     }
 
 }
+
+// DP
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n+1][2*k];
+        for(int[] d: dp){
+            Arrays.fill(d, Integer.MIN_VALUE);
+        }
+        return maxProfitFromIthDay(prices, 2*k, 0, 0, dp);
+    }
+    private int maxProfitFromIthDay(int[] prices, int totalTxns, int i, int txnNumber, int[][] dp){
+        // base case
+        if(i==prices.length || txnNumber == totalTxns){
+            return 0;
+        }
+        if(dp[i][txnNumber] != Integer.MIN_VALUE){
+            return dp[i][txnNumber];
+        }
+        // buy
+        if(txnNumber %2 == 0){
+            int buy = -prices[i] + maxProfitFromIthDay(prices, totalTxns, i+1, txnNumber+1, dp);
+            int notBuy = maxProfitFromIthDay(prices, totalTxns, i+1, txnNumber, dp);
+            return  dp[i][txnNumber] = Math.max(buy, notBuy);
+        }
+        int sell = prices[i] + maxProfitFromIthDay(prices, totalTxns, i+1, txnNumber+1, dp);
+        int notSell = maxProfitFromIthDay(prices, totalTxns, i+1, txnNumber, dp);
+        return dp[i][txnNumber] = Math.max(sell, notSell);
+    }
+
+}
