590b6f7612ab3c371337b876f6ab810676afa8e3
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-10-31 15:11:42 +0530

    Update longest-increasing-subsequence.java

--- leet-code/dynamic-programming/longest-increasing-subsequence.java
+++ leet-code/dynamic-programming/longest-increasing-subsequence.java
@@ -30,3 +30,33 @@ class Solution {
         return dp[i][prevIdx] = lengthOfLISFromIthIdx(nums, i+1, prevIdx, dp);
     }
 }
+
+// DP
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        // Recursion - Top Down Approach
+        // f(i,prevIdx) = LIS from ith index to n-1th index, given prevIdx
+        // right shifted prev Idx
+        int n = nums.length;
+        int[][] dp = new int[n][n+1];
+        for(int[] d: dp){
+            Arrays.fill(d, -1);
+        }
+        return lengthOfLISFromIthIdx(nums, 0, 0, dp);
+    }
+
+    private int lengthOfLISFromIthIdx(int[] nums, int i, int prevIdx, int[][] dp){
+        // base case
+        if(i == nums.length){
+            return 0;
+        }
+        if(dp[i][prevIdx] != -1){
+            return dp[i][prevIdx];
+        }
+        if(prevIdx == 0 || nums[i] > nums[prevIdx-1]){
+            return dp[i][prevIdx] = Math.max(1 + lengthOfLISFromIthIdx(nums, i+1, i+1, dp), 
+                lengthOfLISFromIthIdx(nums, i+1, prevIdx, dp));
+        } 
+        return dp[i][prevIdx] = lengthOfLISFromIthIdx(nums, i+1, prevIdx, dp);
+    }
+}
