9d704b1c19a3d1dd941a15c51a31e7744bca7d0d
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-01 22:43:40 +0530

    Update burst-balloons.java

--- leet-code/dynamic-programming/burst-balloons.java
+++ leet-code/dynamic-programming/burst-balloons.java
@@ -79,3 +79,41 @@ class Solution {
         return dp[i][j] = maxCoins;
     }
 }
+
+// Tabulation
+class Solution {
+    public int maxCoins(int[] nums) {
+        // f(i,j) = max coins from ith index to jth index ballons
+        // if the ballon is bursted, you can't get coins from it
+        // so choose last in a range(i to j) to be bursted
+        // if I decide to burst kth in b/w ith and jth, that means all in i to j
+        // except k are bursted so before k=i-1 and after k= j+1
+        // this would always work, last ->second last -> third last -> first bursted
+        // coins = nums[i-1] * nums[k] * nums[j+1]
+        int n = nums.length + 2;
+        int[] newNums = new int[n];
+        for(int i=1;i<n-1;i++){
+            newNums[i] = nums[i-1];
+        }
+        newNums[0] = 1;
+        newNums[n-1] = 1;
+        int[][] dp = new int[n][n];
+        for(int[] d: dp){
+            Arrays.fill(d, 0);
+        }
+        for(int i=n-2;i>=1;i--){
+            for(int j=i;j<=n-2;j++){
+                if(i>j){
+                    continue;
+                }
+                int maxCoins = Integer.MIN_VALUE;
+                // choose kth indexed ballon to be bursted last in the range
+                for(int k=i;k<=j;k++){
+                    maxCoins = Math.max(maxCoins, newNums[i-1]*newNums[k]*newNums[j+1] + dp[i][k-1] + dp[k+1][j]);
+                }
+                dp[i][j] = maxCoins;
+            }
+        }
+        return dp[1][n-2];
+    }
+}
