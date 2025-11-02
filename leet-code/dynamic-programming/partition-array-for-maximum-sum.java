860bc380c3140fe3cbfc1d48d6ca53cfb1678c07
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-02 13:17:37 +0530

    Update partition-array-for-maximum-sum.java

--- leet-code/dynamic-programming/partition-array-for-maximum-sum.java
+++ leet-code/dynamic-programming/partition-array-for-maximum-sum.java
@@ -19,3 +19,29 @@ class Solution {
         return maxSum;
     }
 }
+
+// DP
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp , -1);
+        return maxSumAfterPartitioningFromIthIdx(arr, 0, k, dp);
+    }
+
+    private int maxSumAfterPartitioningFromIthIdx(int[] arr, int i, int maxLen, int[] dp){
+        if(i == arr.length){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        int maxSum = Integer.MIN_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+        for(int k=i;k<Math.min(i+maxLen, arr.length);k++){
+            maxVal = Math.max(maxVal, arr[k]);
+            maxSum = Math.max(maxSum, (k-i+1)*maxVal + maxSumAfterPartitioningFromIthIdx(arr, k+1, maxLen, dp));
+        }
+        return dp[i] = maxSum;
+    }
+}
