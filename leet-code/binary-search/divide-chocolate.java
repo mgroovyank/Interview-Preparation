8f2ac77e029a30b3ca2c2ed52e5bf7176bc56fce
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-17 16:18:33 +0530

    Create divide-chocolate.java

---
+++ leet-code/binary-search/divide-chocolate.java
@@ -0,0 +1,36 @@
+// https://www.geeksforgeeks.org/dsa/divide-chocolates/
+
+class Solution {
+  public int maximizeSweetness(int[] sweetness, int K){
+    int n = sweetness.length;
+    int low = 1;
+    int high = Arrays.stream(sweetness).sum()/(K+1); // k - friends + 1 me
+    // the minimum chunk sweetness can be 1 to all chunks being of equal sweetness
+    int ans = high;
+    while(low <= high){
+      int mid = low + (high - low)/2;
+      // calculate number of chunks with 'mid' being piece with minimum sweetness
+      int chunks = calculateChunks(sweetness, mid);
+      if(chunks >= K+1){
+        ans = mid;
+        low = mid + 1;
+      }else{
+        high = mid - 1;
+      }
+    }
+    return ans;
+  }
+
+  private int calculateChunks(int[] sweetness, int limit){
+    int chunks = 0;
+    int sum = 0;
+    for(int i=0;i<sweetness.length;i++){
+      sum += sweetness[i];
+      if(sum >= limit){
+        chunks++;
+        sum = 0;
+      }
+    } 
+    return chunks;
+  }
+}
