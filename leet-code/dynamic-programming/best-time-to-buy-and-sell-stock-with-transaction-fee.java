89711647246ada279c9d2469e8a7b7669dd53fa5
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-18 14:09:42 +0530

    Update best-time-to-buy-and-sell-stock-with-transaction-fee.java

--- leet-code/dynamic-programming/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ leet-code/dynamic-programming/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -56,3 +56,37 @@ class Solution {
     }
     
 }
+
+// DP
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        // Recursion - Top Down Approach
+        // Since I know my intial buyFlag=1, I start processing from index=0
+        // f(i, buyFlag) = maxProfitFromIthDay with given buyFlag for ith day
+        // overlapping subproblems - DP
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        for(int[] d: dp){
+            Arrays.fill(d, Integer.MIN_VALUE);
+        }
+        return maxProfitFromIthDay(prices, fee, 0, 1, dp);
+    }
+    private int maxProfitFromIthDay(int[] prices, int fee, int i, int buyFlag, int[][] dp){
+        // base case
+        if(i == prices.length){
+            return 0;
+        }
+        if(dp[i][buyFlag] != Integer.MIN_VALUE){
+            return dp[i][buyFlag];
+        }
+        if(buyFlag == 1){
+            int buy = -prices[i] + maxProfitFromIthDay(prices, fee, i+1, 0, dp);
+            int notBuy = maxProfitFromIthDay(prices, fee, i+1, buyFlag, dp);
+            // if buy is allowed that means, I don't have any stock held, that means I can't sell.
+            return dp[i][buyFlag] = Math.max(buy, notBuy);
+        }
+        int sell = prices[i] - fee + maxProfitFromIthDay(prices, fee, i+1, 1, dp);
+        int notSell = maxProfitFromIthDay(prices, fee, i+1, buyFlag, dp);
+        return dp[i][buyFlag] = Math.max(sell, notSell);
+    }
+}
