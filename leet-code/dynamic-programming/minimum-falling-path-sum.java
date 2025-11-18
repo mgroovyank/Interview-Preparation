5ed02517e36b3c309ee65c0945f65d79558b73eb
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-18 15:27:11 +0530

    Update minimum-falling-path-sum.java

--- leet-code/dynamic-programming/minimum-falling-path-sum.java
+++ leet-code/dynamic-programming/minimum-falling-path-sum.java
@@ -66,3 +66,33 @@ class Solution {
             );
     }
 }
+
+// Tabulation - Bottom Up Approach
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int ans = Integer.MAX_VALUE;
+        int[][] dp = new int[n][n];
+        for(int[] d: dp){
+            Arrays.fill(d, Integer.MAX_VALUE);
+        }
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = matrix[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<n;j++){
+                dp[i][j] = matrix[i][j] + 
+                Math.min(j-1 >=0 ? dp[i+1][j-1] : Integer.MAX_VALUE, 
+                    Math.min(
+                        dp[i+1][j], 
+                        j+1 < n ? dp[i+1][j+1] : Integer.MAX_VALUE
+                    )
+                );
+            }
+        }
+        for(int j=0;j<n;j++){
+            ans = Math.min(ans, dp[0][j]);
+        }
+        return ans;
+    }
+}
