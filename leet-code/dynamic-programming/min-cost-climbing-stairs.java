f8986c2cf5c9abd51ef837a5b0460825a135c061
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-18 11:16:37 +0530

    Update min-cost-climbing-stairs.java

--- leet-code/dynamic-programming/min-cost-climbing-stairs.java
+++ leet-code/dynamic-programming/min-cost-climbing-stairs.java
@@ -25,3 +25,33 @@ class Solution {
             minCostClimbingStairsFromIthIdx(cost, i+2));
     }
 }
+
+// DP
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        // base case for recursion would be when I'm already at top = nth index
+        // f(i) = min cost to reach nth index for ith index
+        // f(i) = cost[i] + Math.min(f(i+1), f(i+2))
+        // recursion tree shows lots of  overlapping subproblems
+        // DP
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[n] = 0;
+        return Math.min(minCostClimbingStairsFromIthIdx(cost, 0, dp), minCostClimbingStairsFromIthIdx(cost, 1, dp));
+    }
+
+    private int minCostClimbingStairsFromIthIdx(int[] cost, int i, int[] dp){
+        // base case
+        if(i > cost.length){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        // do everything on index
+        return dp[i] = cost[i] + Math.min(
+            minCostClimbingStairsFromIthIdx(cost, i+1, dp),
+            minCostClimbingStairsFromIthIdx(cost, i+2, dp));
+    }
+}
