4d6f84478c9d0c21fae6a6cac400e4f16f56b022
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-18 15:18:09 +0530

    Update minimum-falling-path-sum.java

--- leet-code/dynamic-programming/minimum-falling-path-sum.java
+++ leet-code/dynamic-programming/minimum-falling-path-sum.java
@@ -29,3 +29,40 @@ class Solution {
             );
     }
 }
+
+// DP
+
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int ans = Integer.MAX_VALUE;
+        int[][] dp = new int[n][n];
+        for(int[] d: dp){
+            Arrays.fill(d, Integer.MAX_VALUE);
+        }
+        for(int j=0;j<n;j++){
+            ans = Math.min(ans, minFallingPathSumFromIj(matrix, 0, j, dp));
+        }
+        return ans;
+    }
+
+    private int minFallingPathSumFromIj(int[][] matrix, int i, int j, int[][] dp){
+        // base case
+        if(j>=matrix.length || j<0){
+            return Integer.MAX_VALUE;
+        }
+        if(i == matrix.length-1){
+            return matrix[matrix.length-1][j];
+        }
+        if(dp[i][j] != Integer.MAX_VALUE){
+            return dp[i][j];
+        }
+        return dp[i][j] = matrix[i][j] + 
+            Math.min(minFallingPathSumFromIj(matrix, i+1, j-1, dp), 
+                Math.min(
+                    minFallingPathSumFromIj(matrix, i+1, j, dp), 
+                    minFallingPathSumFromIj(matrix, i+1, j+1, dp)
+                )
+            );
+    }
+}
