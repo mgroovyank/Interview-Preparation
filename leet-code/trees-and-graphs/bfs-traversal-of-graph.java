cf045f056385915acbc29896df5be2222b42f50b
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2025-11-19 15:10:19 +0530

    Update bfs-traversal-of-graph.java

--- leet-code/trees-and-graphs/bfs-traversal-of-graph.java
+++ leet-code/trees-and-graphs/bfs-traversal-of-graph.java
@@ -1,6 +1,9 @@
 // https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
 
-// Time Complexity: O(V*E) each node finds degrees of itself
+// Time Complexity: O(V+2E) each node finds degrees of itself , total degrees of graph = 2*E
+// time complexity is not O(V*E)
+// the while loop runs for number of vertices V, for each vertex, for loop runs = number of degrees of that node
+// => V1(Deg1) + V2(Deg2) + ...... + Vn(Degn) = V + 2E
 // Space Complexity: O(V) + O(V) queue + visited array
 class Solution {
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
