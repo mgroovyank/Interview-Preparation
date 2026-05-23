40976adcf41831bc8dd1cfa9a3679e5d5f3453c8
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2026-05-23 19:42:24 +0530

    Update number-of-provinces.java

--- leet-code/trees-and-graphs/number-of-provinces.java
+++ leet-code/trees-and-graphs/number-of-provinces.java
@@ -29,3 +29,50 @@ class Solution {
         }
     }
 }
+
+
+// BFS Solution
+// Time Complexity:
+// every node goes into the queue once, 
+// for each node, we loop over isConnected - n => n + n + n + .... ntimes = n*n = O(N*N) + O(N) <- queue operations on each node
+// so total time complexity of entire bfs is O(N*N)
+// now for outer loop, the calls will be something like this: bfs1 + O(1) + bfs3 + bfs4 + O(1) + ... n nodes => totalbfs + remaining
+// => O(N*N) + O(N)
+class Solution {
+    /**
+    1. Start from an univisited node. Use visited[] to maintain that.
+    2. From that node, try doing bfs
+    3. After doing bfs, since you must have visited all the direct/indirect connected nodes, you have visited
+       all nodes part of same province, so increment province count.
+    4. Continue from 2 with next unvisited node.
+    5. In BFS, I would need to know the negihbors for each node, so I can use adjacencyMatrix representation
+     */
+    public int findCircleNum(int[][] isConnected) {
+        int numberOfProvinces = 0;
+        int n = isConnected.length;
+        int[] visited = new int[n];
+        for(int i=0;i<n;i++){
+            if(visited[i] == 0){
+                bfs(isConnected, i, visited);
+                numberOfProvinces++;
+            }
+        }
+        return numberOfProvinces;
+    }
+
+    private void bfs(int[][] isConnected, int i, int[] visited){
+        Queue<Integer> q = new ArrayDeque<>();
+        visited[i] = 1;
+        q.add(i);
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            int[] neighbors = isConnected[curr];
+            for(int j=0;j<neighbors.length;j++){
+                if(neighbors[j] == 1 && visited[j]==0){
+                    visited[j] = 1;
+                    q.add(j);
+                }
+            }
+        }
+    } 
+}
