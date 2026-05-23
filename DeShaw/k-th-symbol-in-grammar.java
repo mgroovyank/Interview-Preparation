1833eaa26839b45a7c55be31187a8c7684f1126e
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2026-05-23 16:56:13 +0530

    Update k-th-symbol-in-grammar.java

--- DeShaw/k-th-symbol-in-grammar.java
+++ DeShaw/k-th-symbol-in-grammar.java
@@ -23,6 +23,17 @@ class Solution {
        -> Time Complexity: O(N)
           -> not exponential because each call to (n,k) makes exactly one recursive call to (n-1, prevK)
        -> Space Complexity: O(N) 
+
+    5. Iterative Approach(Bottom Up Approach)
+       -> 1-indexed: [0,1,1,0,1,0,0,1]
+          -> Create an array of length k
+          -> at 1-index put value as 0
+          -> start processing array from 2nd index
+          -> at ith index
+             -> calculate prevK as per same logic used in recursion
+             -> also use same logic to determine digit at current ith index as used in recursion
+        -> THROWS OUT OF MEMORY ERROR as max K= 434991989 * 4 bytes(int) ~ 1.75GB
+        -> JVM doesn't have that much heap size 
      */
 
     
@@ -48,6 +59,27 @@ class Solution {
             return digit;
         }
 
+    }
+
+}
+
+// Iterative Approach - THROWS OUT OF MEMORY ERROR
+public int kthGrammar(int n, int k) {
+        int[] result = new int[k+1];
+        result[1] = 0;
+        for(int i=2;i<=k;i++){
+            if(i%2 == 0){
+                int digit = result[i/2];
+                if(digit == 0){
+                    result[i] = 1;
+                    continue;
+                }
+                result[i] = 0;
+            }else {
+                result[i] = result[(i-1)/2 + 1];
+            }
+        }
+        return result[k];
     }
 
 }
