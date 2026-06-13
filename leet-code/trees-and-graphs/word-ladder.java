e78b52d7019012a1d4b73a01c74bf0dbec5dd982
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2026-06-13 16:54:59 +0530

    Update word-ladder.java

--- leet-code/trees-and-graphs/word-ladder.java
+++ leet-code/trees-and-graphs/word-ladder.java
@@ -1,4 +1,55 @@
 // https://leetcode.com/problems/word-ladder/
+
+class Solution {
+    /**
+    1. At any point, i can try changing each character in the word to any of the a-z chars
+       only if after change the word exists in wordList
+    2. If after change, word is not word list, stop there
+    3. If after change, word exists in word list, move ahead and repeat
+    4. at some point if you find your endWord, that's a possible answer
+    5. but maybe there are other better ways as well to reach that word, so need to explore
+       all cases
+    6. also you have to do BFS in this case - as shortest path is needed
+    7. So I think we have a Map<String, Set<String>> where key in (beginWord, endWord, ...wordList)
+    8. and then just do a BFS ans store distance of each word when it is visited
+    9. at the end of BFS, if the endWord is not visited, return 0. otherwise return distance[endWord]
+    10. Number of vertices = number of words, number of edges=   
+    11. The below implemented approach is little simpler and efficient than the described approach.
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>(wordList);
+        Deque<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(beginWord, 1));
+        while(!q.isEmpty()){
+            Pair curr = q.remove();
+            if(curr.s.equals(endWord)){
+                return curr.dist;
+            }
+            for(int i=0;i<curr.s.length();i++){
+                StringBuilder temp = new StringBuilder(curr.s);
+                for(int j=0;j<26;j++){
+                    temp.setCharAt(i, (char)('a'+ j));
+                    String nextWord = temp.toString();
+                    if(words.contains(nextWord)){
+                        q.add(new Pair(nextWord, curr.dist + 1));
+                        words.remove(nextWord);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    class Pair{
+        String s;
+        int dist;
+        Pair(String s, int dist){
+            this.s = s;
+            this.dist = dist;
+        }
+    }
+}
+
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordListSet = new HashSet<>(wordList);
