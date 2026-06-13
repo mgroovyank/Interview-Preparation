1e4326be02f551aebb6acd4014862e72d0a2559f
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2026-06-13 19:56:23 +0530

    Create djikstra-algorithm.java

---
+++ leet-code/trees-and-graphs/djikstra-algorithm.java
@@ -0,0 +1,60 @@
+// https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
+
+// DOES NOT WORK WITH NEGATIVE WEIGHTS
+
+// Time Complexity:
+// Space Complexity: O(V*Number of edges)
+class Solution {
+    public int[] dijkstra(int V, int[][] edges, int src) {
+        List<List<NodeWeight>> graph = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            graph.get(u).add(new NodeWeight(v, w));
+            graph.get(v).add(new NodeWeight(u, w));
+        }
+        
+        int[] shortestDist = new int[V];
+        Arrays.fill(shortestDist, Integer.MAX_VALUE);
+        
+        PriorityQueue<NodeWeight> pq = new PriorityQueue<>();
+        pq.add(new NodeWeight(src, shortestDist[src] = 0));
+        
+        
+        while(!pq.isEmpty()){
+            NodeWeight nw = pq.poll();
+            int currNode = nw.node;
+            int currWeight = nw.weight;
+            
+            List<NodeWeight> neighbors = graph.get(currNode);
+            for(NodeWeight neighbor: neighbors){
+                int nextNode = neighbor.node;
+                int nextWeight = currWeight + neighbor.weight;
+                if(nextWeight < shortestDist[nextNode]){
+                    shortestDist[nextNode] = nextWeight;
+                    pq.add(new NodeWeight(nextNode, nextWeight));
+                }
+            }
+        }
+        return shortestDist;
+    }
+    
+    class NodeWeight implements Comparable<NodeWeight>{
+        int node;
+        int weight;
+        
+        NodeWeight(int node, int weight){
+            this.node = node;
+            this.weight = weight;
+        }
+        
+        public int compareTo(NodeWeight nw){
+            return Integer.compare(this.weight, nw.weight);
+        }
+    }
+}
